8584037f85ffd15b23704894dea5226cd5273456
usuario <jair01@hotmail.com>
2018-11-26 17:33:55 -0500

    second page

---
+++ apps/flujo/properties.py
@@ -0,0 +1,23 @@
+DIA="DIA"
+MES = "MES"
+HORA ="HORA"
+SUMA="SUMA"
+RESTA="RESTA"
+LUNES="Lunes"
+MARTES="Martes"
+
+TIPO_TIEMPO =(
+    (HORA,"Hora")
+    (DIA, "Dia")
+    (MES, "Mes")
+)
+
+TIPO_CATEGORIA =(
+    (SUMA,"Suma")
+    (RESTA, "Resta")
+)
+
+DIAS_SEMANA =(
+    (LUNES,"Lunes")
+    (MARTES, "Martes")
+)
